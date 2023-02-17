74e88b049f39494112d77c3addd6ae8026fcb73c
ceroxtv <99655407+ceroxtv@users.noreply.github.com>
2023-02-17 13:38:13 +0100

    Delete spring-boot-backend-eventos directory

--- spring-boot-backend-eventos/src/main/java/com/fran/springboot/backend/eventos/repositorios/IMensaje.java
+++
@@ -1,9 +0,0 @@
-package com.fran.springboot.backend.eventos.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fran.springboot.backend.eventos.entidades.Mensaje;
-
-public interface IMensaje extends JpaRepository<Mensaje,Long> {
-
-}
