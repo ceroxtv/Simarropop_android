f18c9621825ae9933e5c708967b22f4fb20f3140
ceroxtv <99655407+ceroxtv@users.noreply.github.com>
2023-02-22 18:12:21 +0100

    Delete spring-boot-backend-eventos directory

--- spring-boot-backend-eventos/src/main/java/com/fran/springboot/backend/eventos/repositorios/ICategoria.java
+++
@@ -1,9 +0,0 @@
-package com.fran.springboot.backend.eventos.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fran.springboot.backend.eventos.entidades.Categoria;
-
-public interface ICategoria extends JpaRepository<Categoria,Long> {
-
-}
